2253f7e8ae88ff0f6a4b9fe32c0378c34cdaf749
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-07-02 10:34:29 +0900

    ページをわけた

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-// import "package:./TodoList.dart"
+import "./AddTodoPage.dart";
+
 void main() {
   // 最初に表示するWidget
   runApp(MyTodoApp());
@@ -71,67 +72,6 @@ class _TodoListPageState extends State<TodoListPage> {
         },
         child: Icon(Icons.add),
       ),
-    );
-  }
-}
-
-class TodoAddPage extends StatefulWidget {
-  @override
-  _TodoAddPageState createState() => _TodoAddPageState();
-}
-
-class _TodoAddPageState extends State {
-  String _text = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('リストを追加', style: TextStyle(color: Colors.black)),
-          backgroundColor: Colors.grey[200]),
-      body: Container(
-          padding: EdgeInsets.all(64),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(_text, style: TextStyle(color: Colors.blue)),
-                TextField(onChanged: (String value) {
-                  setState(() {
-                    _text = value;
-                  });
-                }),
-                const SizedBox(height: 8),
-                Container(
-                  // 横幅いっぱいに広げる
-                  width: double.infinity,
-                  // リスト追加ボタン
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Colors.blueAccent.shade400),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pop(_text);
-                    },
-                    child: Text('リスト追加', style: TextStyle(color: Colors.white)),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Container(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Colors.red.shade400),
-                      ),
-                      onPressed: () {
-                        // "pop"で前の画面に戻る
-                        //
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('キャンセル'),
-                    ))
-              ])),
     );
   }
 }
